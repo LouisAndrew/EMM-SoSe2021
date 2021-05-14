d96225e80749955df415f15be94f68bdf5594813
Louis Andrew <louis.andrew@share-now.com>
2021-05-14 10:39:53 +0200

    feat(scene): change scene name to abgabe3(ABGABE-3)

--- Assets/Scripts/ButtonEventHandler.cs
+++ Assets/Scripts/ButtonEventHandler.cs
@@ -5,26 +5,24 @@ using Vuforia;
 public class ButtonEventHandlder : MonoBehaviour
 {
 
-  public GameObject vBtn;
+ public GameObject vBtn;
 
 
-  // Use this for initialization
-  void Start()
-  {
-    vBtn = GameObject.Find("Btn");
-    vBtn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
-    vBtn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+ // Use this for initialization                 
+ void Start()
+ {
+  vBtn = GameObject.Find("Btn");
+  vBtn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+  vBtn.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+ }
 
+ public void OnButtonPressed(VirtualButtonBehaviour vb)
+ {
 
-  }
+ }
 
-  public void OnButtonPressed(VirtualButtonBehaviour vb)
-  {
+ public void OnButtonReleased(VirtualButtonBehaviour vb)
+ {
 
-  }
-
-  public void OnButtonReleased(VirtualButtonBehaviour vb)
-  {
-
-  }
+ }
 }
